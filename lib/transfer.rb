e2f7cbb8f3094dfe84c761bf0b0d61b9ad6e436c
derrick b <dbozich1@gmail.com>
2018-06-02 09:06:44 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -46,21 +46,21 @@ end
 # class BankAccount
 #   attr_accessor :balance, :status
 #   attr_reader :name
-# 
+#
 #   def initialize(name)
 #     @name = name
 #     @balance = 1000
 #     @status = "open"
 #   end
-# 
+#
 #   def deposit(amount)
 #     @balance += amount
 #   end
-# 
+#
 #   def display_balance
 #     "Your balance is $#{@balance}."
 #   end
-# 
+#
 #   def valid?
 #     if @status == "open" && @balance > 0
 #       true
@@ -68,13 +68,13 @@ end
 #       false
 #     end
 #   end
-# 
+#
 #   def close_account
 #     @status = "closed"
 #   end
-# 
+#
 # end
-# 
+#
 # broke_boi = BankAccount.new("Broke Boi")
 # gertrude = BankAccount.new("Gertrude")
 # transfer = Transfer.new(broke_boi, gertrude, 50)
