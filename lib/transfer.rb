60718aacae19f08c4442f14abab86cf824edf9ba
derrick b <dbozich1@gmail.com>
2018-06-02 08:31:01 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,6 +15,7 @@ class Transfer
   def execute_transaction
     if @status == "pending"
       if self.valid? == false
+      @status = "rejected"
       "Transaction rejected. Please check your account balance."
       end
       @sender.balance -= amount
